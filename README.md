5ccef4d352c50599226193078dffaa57246130f5
Nhung Tran <nhungtrannn@gmail.com>
2019-02-08 10:07:44 -0600

    work on more JavaScript

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# projectSimon
+# projectSid
 Four colored buttons light up in a specific pattern. After displaying the pattern, the player must repeat the pattern by clicking the buttons in proper order. The pattern gets longer each time the player completes the pattern. If the player presses a wrong button, the game ends.
 
+
 Wireframes 
+
 
 As a player, I am presented with four colored buttons that light up in a specific pattern. 
 
@@ -22,11 +24,13 @@ I can play in strict mode where if I press the wrong button, it notifies me that
 I can win the game by getting a series of 20 steps correct. I am notified of victory, then the game starts over. 
 
 
+
 pseudocode
+
 
 when the player clicks on start, the computer will light up the first color
 
-if the player clicks the right color, the computer will display the first color and then another 
+if the player clicks the right color, the computer will display the first color and then second color
 
 if the player clicks the wrong color, the computer will notify you that you've done so
 
--- js/main.js
+++ js/main.js
@@ -1,39 +1,112 @@
 /*----- constants -----*/ 
-
+const one = document.getElementsByClassName("blue");
+const two = document.getElementsByClassName("red");
+const three = document.getElementsByClassName("green");
+const four = document.getElementsByClassName("yellow");
 
 
 
 /*----- app's state (variables) -----*/
-// winningPattern
-// losingPattern
-// winGame
-let start 
+var userClicks = [];
 
+var compClicks = [];
+
+var level = 0;
+
+var color = 0;
 
 /*----- cached element references -----*/
-const topleft = document.getElementById("topleft");
-const topleftClass = document.getElementById('topleft').className;
-const topright = document.getElementById("topright");
-const topleftClass = document.getElementById('topright').className;
-const bottomleft = document.getElementById("bottomleft");
-const topleftClass = document.getElementById('bottomleft').className;
-const bottomright = document.getElementById("bottomright");
-const topleftClass = document.getElementById('bottomright').className;
+
+// const topleft = document.getElementById("topleft");
+// const topleftClass = document.getElementById('topleft').className;
+// const topright = document.getElementById("topright");
+// const toprightClass = document.getElementById('topright').className;
+// const bottomleft = document.getElementById("bottomleft");
+// const bottomleftClass = document.getElementById('bottomleft').className;
+// const bottomright = document.getElementById("bottomright");
+// const bottomrightClass = document.getElementById('bottomright').className;
 
 
 
 /*----- event listeners -----*/ 
 
-//topleft event listener
+//topleft, topright, bottomleft, bottomright event listener
 topleft.addEventListener('click', topLeftCallback);
-
-
+topright.addEventListener('click', topRightCallback);
+bottomleft.addEventListener('click', bottomLeftCallback);
+bottomright.addEventListener('click', bottomRightCallback);
 
 /*----- functions -----*/
-//topleft callback
+//start game 
+let topLeftColor = '#6495ED';
+let topRightColor = '#DC143C';
+let bottomLeftColor = '#2E8B57';
+let bottomRightColor = '#FFFF00'; 
+
+
+$(document).ready(function() {
+    $(".turn").text("");
+    $(".button").click(function() {
+        // console.log('hello');
+        level++;
+        startSequence();
+       changeColor('#topright', topRightColor)
+    })
+})
+
+function changeColor(id, color) {
+    $(id).css({'background-color' : 'white'})
+
+    setTimeout(function(){
+        console.log('hellp')
+        $(id).css({'background-color' : color})
+    }, 500);  
+}
+
+function startSequence() {
+    $("#turn").text(level);
+}
+
+
+
+
+//topleft, topright, bottomleft, bottomright callback
 function topLeftCallback (event) {
     console.log(document.getElementById('topleft').className);
     console.log(event.target.style.background);
-    event.target.style.background = "purple";
-    
+    if (event.target.style.background === 'white') {
+        event.target.style.background = "#6495ED";
+    } else {
+        event.target.style.background = "white";
+    }
+}
+
+function topRightCallback (event) {
+    console.log(document.getElementById('topright').className);
+    console.log(event.target.style.background);
+    if (event.target.style.background === 'white') {
+        event.target.style.background = "#DC143C";
+    } else {
+        event.target.style.background = "white";
+    }
+}
+
+function bottomLeftCallback (event) {
+        console.log(document.getElementById('bottomleft').className);
+        console.log(event.target.style.background);
+        if (event.target.style.background === 'white') {
+            event.target.style.background = "#2E8B57";
+        } else {
+            event.target.style.background = "white";
+        }
+}
+
+function bottomRightCallback (event) {
+            console.log(document.getElementById('bottomright').className);
+            console.log(event.target.style.background);
+            if (event.target.style.background === 'white') {
+                event.target.style.background = "#FFFF00";
+            } else {
+                event.target.style.background = "white";
+            }
 }
